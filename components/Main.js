c1ba5da518343c264e07078f1fca40f501c8e631
Sarfaraz Ahmed <sarfaraz.ahmed008@hotmail.com>
2018-10-06 01:31:23 +0600

    functionality

--- components/Main.js
+++ components/Main.js
@@ -7,30 +7,58 @@ import {
 } from 'react-native';
 
 export default class Main extends Component {
-    state = {
-        number: '',
+    constructor(props){
+        super(props);
+        this.state = {
+            number: '',
+            result: '',
+        }
+        this.operators = ['C','D', '+', '-', '*', '/'];
     }
     getNumber = (num) => {
         const {number} = this.state;
-        switch (num){
-            case('='): {
-                this.getResult();
-                return;
-            }
+        if (num == '='){
+            return this.validate() && this.getResult();
         }
         this.setState({number: number+num});
+
     };
     getOperator = (opr) => {
         switch(opr){
-            case ('D'): {
+            case 'C':
+                this.setState({number: ''});
+                this.setState({result: ''});
+                break;
+            case 'D': 
                 let num =  this.state.number.split('');
                 num.pop();
                 this.setState({number: num.join('')});
-            }
+                break;
+            case '+':
+            case '-':
+            case '*':
+            case '/':
+                if(this.state.number == '') return;
+                let lastChar = this.state.number.split('').pop();
+                if(this.operators.indexOf(lastChar) > 0) return;
+                this.setState({number: this.state.number+opr});
+
         }
     };
     getResult(){
-        //parse and calculate
+        const text = this.state.number;
+        this.setState({result: eval(text)});
+    }
+    validate(){
+        const num = this.state.number;
+        switch(num.slice(-1)){
+            case '+':
+            case '-':
+            case '*':
+            case '/':
+                return false;
+        }
+        return true;
     }
     render(){
         const numbers = [[7,8,9],[4,5,6],[1,2,3],['.',0,'=']];
@@ -39,33 +67,31 @@ export default class Main extends Component {
             let row = [];
             for (let j=0; j<3; j++){
                 row.push(
-                    <TouchableOpacity style={styles.btnStyle} onPress={()=>this.getNumber(numbers[i][j])}>
-                        <Text style={[styles.btnText, styles.color]}>{numbers[i][j]}</Text>
+                    <TouchableOpacity key={numbers[i][j]} style={styles.btnStyle} onPress={()=>this.getNumber(numbers[i][j])}>
+                        <Text style={[styles.btnText, styles.color, styles.fs]}>{numbers[i][j]}</Text>
                     </TouchableOpacity>
                 );
             }
             rows.push(
-                <View style = {styles.rowStyle}>{row}</View>
+                <View key={i} style = {styles.rowStyle}>{row}</View>
             );
         }
-        const operators = ['D', '+', '-', '*', '/'];
         let ops = [];
-        for(let i=0; i<5; i++){
+        for(let i=0; i<6; i++){
             ops.push(
-                <TouchableOpacity style={styles.btnStyle} onPress={() => this.getOperator(operators[i]) }>
-                        <Text style={[styles.btnText, styles.color]}>{operators[i]}</Text>
+                <TouchableOpacity key={this.operators[i]} style={styles.btnStyle} onPress={() => this.getOperator(this.operators[i]) }>
+                        <Text style={[styles.btnText, styles.color, styles.fs]}>{this.operators[i]}</Text>
                 </TouchableOpacity>
             );
         }
-
-
         return (
             <View style={styles.container}>
+                <View style={styles.header}><Text style={styles.headerText}>SA Calculator</Text></View>
                 <View style={styles.displayText}>
-                    <Text style={[styles.displayTextStyle, styles.color ]}>{this.state.number}</Text>
+                    <Text style={[styles.displayTextStyle, styles.fs]}>{this.state.number}</Text>
                 </View>
                 <View style={styles.displayResult}>
-                    <Text style={[styles.displayResultStyle, styles.color]}>23432</Text>
+                    <Text style={[styles.displayResultStyle, styles.fs]}>{this.state.result}</Text>
                 </View>
                 <View style={styles.buttons}>
                     <View style={styles.numbers}>
@@ -86,16 +112,16 @@ const styles = StyleSheet.create({
     },
     displayText: {
         flex: 2,
-        backgroundColor: 'blue',
+        backgroundColor: '#fff',
         justifyContent: 'center',
         alignItems: 'flex-end',
     },
     color: {
-        color: 'white',
+        color: '#fff',
     },
     displayResult: {
         flex: 1,
-        backgroundColor: 'green',
+        backgroundColor: '#fff',
         justifyContent: 'center',
         alignItems: 'flex-end',
     },
@@ -105,34 +131,47 @@ const styles = StyleSheet.create({
     },
     numbers: {
         flex: 3,
-        backgroundColor: 'red',
+        backgroundColor: '#636363',
     },
     operators: {
         flex: 1,
-        backgroundColor: 'black',
+        backgroundColor: '#61c4a8',
     },
     displayTextStyle: {
         fontSize: 37,
-
+        color: 'black',
     },
     displayResultStyle: {
         fontSize: 26,
+        color: 'black',
     },
     btnStyle: {
         justifyContent: 'center',
         alignItems: 'center',
         alignSelf: 'stretch',
         flex: 1,
-        borderRadius: 30,
-        backgroundColor: 'blue',
+        //borderRadius: 30,
+        elevation: 10,
+        
     },
     btnText: {
         fontSize: 40,
-        color: 'white',
+        color: '#fff',
         textAlign: 'center',  
     },
     rowStyle: {
         flex: 1,
         flexDirection: 'row',
+    },
+    fs: {
+        fontWeight: "100",
+        fontFamily: 'sans-serif',
+    },
+    header: {
+        justifyContent: 'center',
+        alignSelf:'center',
+    },
+    headerText: {
+        color: 'black',
     },
 });
